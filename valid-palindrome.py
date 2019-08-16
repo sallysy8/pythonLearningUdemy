5859f4ae971a530ede8bd425c6652b3703e30938
Sally <shenyingyy@gmail.com>
2019-08-15 22:15:33 -0700

    Update valid-palindrome.py

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -12,10 +12,13 @@ def isPalindrome(s):
         i = 0
         j = new_s[-1]
         while i < j:
-            while new_s[i].isalnum() is True:
+            while new_s[i].isalnum() is False
                 i+=1
-            while new_s[j].isalnum() is True:
+            while new_s[j].isalnum() is False:
                 j-=1
             if new_s[i] != new_s[j]:
                 return False
+            else:
+                i+=1
+                j+=1
         return True
