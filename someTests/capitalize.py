59c638ddaecd2eda6234aeb5076a0494453836f4
sallysy8 <sallysy8@hotmail.com>
2019-08-18 20:26:10 -0700

    some simply test

---
+++ someTests/capitalize.py
@@ -0,0 +1,11 @@
+'''
+capitalize the first letter
+first use method capitalize(), It doesn't take all single word capitalized,
+so change the method to title()
+'''
+
+
+def cap_text(text):
+
+    return text.title()
+
